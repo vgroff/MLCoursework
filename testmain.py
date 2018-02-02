96ea3b531e32e3d8d461be4a29f3051d38c3997c
vincent <vg1416@ic.ac.uk>
2018-02-02 14:11:11 +0000

    Model is classifying

--- testmain.py
+++ testmain.py
@@ -11,7 +11,7 @@ from scipy.io import *
 from graphviz import *
 import pandas as pda
 import numpy as np
-import Node
+import Model
 import Tree
 
 
@@ -22,11 +22,7 @@ def Load_panda(Filename, Variable):
 
     return output
 
-class Model():
-    def __init__(self, data):
-        dataLabels = data[["label"]]
-        print(dataLabels)
-        
+
 # read in the files and labels from the .mat into Panda arrays
 
 clean_df = Load_panda("./Data/cleandata_students.mat",'x')
@@ -39,8 +35,10 @@ new_clean_df = clean_df.loc[clean_df[1] == 1]
 #clean_df.to_csv("clean_data1.csv", sep=',')
 #print(clean_df.iloc[:,19:25])
 #tree_array = []
-tree = Tree.Tree(clean_df,5)
-Tree.print_tree(tree.root_node)
+
+#tree = Tree.Tree(clean_df,5)
+#Tree.print_tree(tree.root_node)
+
 #for i in range(0,6):
 #    tree = Tree.Tree(clean_df,i)
 #    tree_array.append(tree)
@@ -58,7 +56,9 @@ test_row = clean_df.iloc[13,:]
 #
 #
 #print(Tree.confusion_matrix(test_col,test_col))
-print(tree.classify(test_row))
+
+#print(tree.classify(test_row))
+
 #test = Tree.test_sets(clean_df)
 
 #print_test = tree.root_nodes[0]
@@ -74,7 +74,9 @@ print(tree.classify(test_row))
 #print("original entropy: ",Node.entropy(clean_df,1))
 #Node.info_gain(clean_df, 1)
 
-model = Model(clean_df)
+model = Model.Model(clean_df)
+print("testing", test_row)
+print(model.classify(test_row))
 
 # print(new_clean_df.loc[:,1].var())
 # print(clean_df.loc[clean_df[1] == 1].shape[0])
